05bf8cf79c13cf98c2c546d84fc675fdbc14f51b
Denis Skripnichenko <dskripnichenko@clevertec.ru>
2018-12-21 05:13:56 +0300

    Initial Commit

---
+++ RSSViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSViewer
+//
+//  Created by Denis Skripnichenko on 21.12.2018.
+//  Copyright © 2018 Denis Skripnichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
